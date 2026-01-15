c5ccf412508a54b259c74d944d1750e03c93cb35
quh <quhxuxm_quh@163.com>
2026-01-15 15:29:40 +0800

    init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,18 +6,27 @@ pub mod process;
 pub mod repo;
 pub mod runtime;
 
+use std::alloc::System;
+
 use command::{
     exec_build_process, get_github_runtime_detail, get_project_code, get_project_runtime_detail,
     get_project_runtime_summaries, save_project,
 };
+use tauri::async_runtime;
+use tokio::runtime::{Builder, Runtime};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-pub async fn run() {
+pub fn run() {
+    let parallelism = std::thread::available_parallelism().expect("Fail to get system parallelism");
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(parallelism)
+        .enable_all()
+        .build()
+        .expect("Fail to create async runtime");
     tracing_subscriber::fmt().init();
-
+    async_runtime::set(runtime);
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
